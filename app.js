467ae828948852159691611311e97643e7c16f17
thirupathi14 <tirupatireddyj@gmail.com>
2020-06-04 10:43:26 -0700

    added new button types

---
+++ app.js
@@ -0,0 +1,7 @@
+
+function executeOnClick() {
+   var frame=function executeOnClick(){
+    $('body').append('<iframe name="your_frame_name" src="www.google.com" height=200 width=200 > </iframe>');
+      }
+    document.getElementById("screen").appendChild=frame;
+}
